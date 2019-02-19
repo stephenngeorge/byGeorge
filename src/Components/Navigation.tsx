0100092d8c094f4a9889ff9448775f4f728af124
Stephen George <stephenngeorge@gmail.com>
2019-02-19 15:25:37 +0000

    nav bar and nav bar styles

---
+++ src/Components/Navigation.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+// import assets
+import {
+    about_icon,
+    facebook_logo,
+    instagram_logo,
+    linkedin_logo,
+    portfolio_icon,
+    twitter_logo
+} from '../assets'
+
+export default () => (
+    <ul className='navigation'>
+        <li className='navigation__item'>
+            <Link   to='/portfolio'
+                    className='navigation__link'
+                    id='portfolio-link'
+            >
+                <img src={ portfolio_icon } alt='portfolio' />
+            </Link>
+            <p className='navigation__item--tag' id='portfolio-tag'>portfolio</p>
+        </li>
+        <li className='navigation__item'>
+            <Link   to='/about'
+                    className='navigation__link'
+                    id='about-link'
+            >
+                <img src={ about_icon } alt='about' />
+            </Link>
+            <p className='navigation__item--tag' id='about-tag'>about</p>
+        </li>
+        <li className='navigation__item'>
+            <a  href='https://www.facebook.com/stephen.n.george'
+                className='navigation__link'
+                id='facebook-link'
+                target='_blank'
+            >
+                <img src={ facebook_logo } alt='facebook' />
+            </a>
+            <p className='navigation__item--tag'>@stephen.n.george</p>
+        </li>
+        <li className='navigation__item'>
+            <a  href='https://twitter.com/StephennGeorge'
+                className='navigation__link'
+                id='twitter-link'
+                target='_blank'
+            >
+                <img src={ twitter_logo } alt='twitter' />
+            </a>
+            <p className='navigation__item--tag'>@stephenngeorge</p>
+        </li>
+        <li className='navigation__item'>
+            <a  href='https://www.instagram.com/stephengeorge1989/'
+                className='navigation__link'
+                id='instagram-link'
+                target='_blank'
+            >
+                <img src={ instagram_logo } alt='instagram' />
+            </a>
+            <p className='navigation__item--tag'>@stephengeorge1989</p>
+        </li>
+        <li className='navigation__item'>
+            <a  href='https://www.linkedin.com/in/stephen-george-1a7a1445/'
+                className='navigation__link'
+                id='linkedin-link'
+                target='_blank'
+            >
+                <img src={ linkedin_logo } alt='linkedin' />
+            </a>
+            <p className='navigation__item--tag'>stephengeorge</p>
+        </li>
+    </ul>
+)
---
+++ src/assets/index.tsx
@@ -0,0 +1,17 @@
+import about_icon from './about_icon.png'
+import facebook_logo from './facebook_logo.png'
+import instagram_logo from './instagram_logo.png'
+import linkedin_logo from './linkedin_logo.png'
+import portfolio_icon from './portfolio_icon.png'
+import profile_pic from './profile_pic.jpg'
+import twitter_logo from './twitter_logo.png'
+
+export {
+    about_icon,
+    facebook_logo,
+    instagram_logo,
+    linkedin_logo,
+    portfolio_icon,
+    profile_pic,
+    twitter_logo
+}
