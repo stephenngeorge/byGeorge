e624f1d455ae0828e216899675bfb43138aac971
Stephen George <stephenngeorge@gmail.com>
2019-03-03 22:57:39 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/byGeorge/precache-manifest.86707df5d356f3682654a7b95b928639.js"
+  "/bygeorge/precache-manifest.fed62a556a4146042a9309ba405982ca.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/byGeorge/index.html", {
+workbox.routing.registerNavigationRoute("/bygeorge/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
